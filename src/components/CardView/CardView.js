d16acc970c247ab9353622b97110c096c27175d7
Jibola <omojibolaorija@gmail.com>
2023-02-02 01:47:52 +0000

    complete

--- src/components/CardView/CardView.js
+++ src/components/CardView/CardView.js
@@ -1,45 +1,66 @@
-import Card from '../Card/Card';
-import { CardSwiper } from 'react-card-rotate-swiper';
+import React, { useState } from 'react';
+import toast from 'react-hot-toast';
+import TinderCard from 'react-tinder-card';
 
-const CardView = () => {
-  const cards = [
-    {
-      image: 'https://img.icons8.com/color/452/GeeksforGeeks.png',
-      color: '#55ccff',
-    },
-    {
-      image: 'https://img.icons8.com/color/452/GeeksforGeeks.png',
-      color: '#e8e8e8',
-    },
-    {
-      image: 'https://img.icons8.com/color/452/GeeksforGeeks.png',
-      color: '#0a043c',
-    },
-    {
-      image: 'https://img.icons8.com/color/452/GeeksforGeeks.png',
-      color: 'black',
-    },
-  ];
+const images = [
+  '/poki-1.png',
+  '/poki-2.jpeg',
+  '/poki-3.jpeg',
+  '/poki-4.jpeg',
+  '/poki-5.jpeg',
+];
 
-  const handleSwipe = (d) => {
-    //fill this your callback
+function CardView({ characters, displayStats }) {
+  const [lastDirection, setLastDirection] = useState();
+  const [randomImg, setRandomImg] = useState(0);
+
+  const swiped = (direction, nameToDelete) => {
+    console.log('removing: ' + nameToDelete);
+    setLastDirection(direction);
   };
+
+  const outOfFrame = (name) => {
+    console.log(name + ' left the screen!');
+  };
+
   return (
-    <>
-      <div className='App'>
-        <CardSwiper
-          onSwipe={handleSwipe}
-          className={'swiper'}
-          contents={cards.map((card) => (
-            <div>
-              <Card image={card.image} />
-              <Card image={card.image} />
+    <div>
+      <div className='cardContainer'>
+        {characters.slice(0, 20)?.map((character) => (
+          <TinderCard
+            className='swipe'
+            key={character.name}
+            onSwipe={(dir) => {
+              swiped(dir, character.name);
+              setRandomImg(Math.floor(Math.random() * images.length));
+            }}
+            onCardLeftScreen={() => outOfFrame(character.name)}
+            preventSwipe={['up', 'down']}
+          >
+            <div className='card'>
+              <img src={images[randomImg]} alt='' />
+
+              <h3>{character.name}</h3>
+
+              <button
+                className='card-btn'
+                onClick={(e) => {
+                  toast.success(<p>You just liked {character.name}</p>);
+                }}
+              >
+                Like character
+              </button>
             </div>
-          ))}
-        />
+          </TinderCard>
+        ))}
       </div>
-    </>
+      {/* {lastDirection ? (
+        <h2 className='infoText'>You swiped {lastDirection}</h2>
+      ) : (
+        <h2 className='infoText' />
+      )} */}
+    </div>
   );
-};
+}
 
 export default CardView;
--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,6 +1,13 @@
 import '@/styles/globals.css';
 import '@/styles/spinner.css';
+import '@/styles/card.css';
+import { Toaster } from 'react-hot-toast';
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Component {...pageProps} />
+      <Toaster />
+    </>
+  );
 }
